20f58c82db7c50f063a402b64e689a04fe9b54d9
kwkrass <karsten.wysk@gmail.com>
2026-03-09 15:59:57 +0100

    dont create a new contact twice

--- components/ContactsView/EditContact.js
+++ components/ContactsView/EditContact.js
@@ -456,7 +456,7 @@ export default class EditContact extends Component {
                             projectId={projectId}
                             forceTriggerEnterActionForBreakLines={this.enterActionKey}
                             disabledEdition={!loggedUserCanUpdateObject}
-                            onMentionSelected={this.onMentionSelected}
+                            onMentionSelected={isNew ? undefined : this.onMentionSelected}
                         />
                     )}
                 </View>
