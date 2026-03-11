0ad4aad57ac5b65e68b280af2b828db93cfbe76c
kwkrass <karsten.wysk@gmail.com>
2026-03-11 10:42:53 +0100

    i can properly press return in the system prompt for the assistant

--- components/UIComponents/FloatModals/AssistantInstructionsModal/EditForm.js
+++ components/UIComponents/FloatModals/AssistantInstructionsModal/EditForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import Button from '../../../UIControls/Button'
@@ -20,21 +20,6 @@ export default function EditForm({
     const done = () => {
         setInstructions(text.trim())
     }
-
-    const onKeyDown = event => {
-        const { key } = event
-        if (key === 'Enter' && !event.shiftKey) {
-            done()
-            event.preventDefault()
-        }
-    }
-
-    useEffect(() => {
-        document.addEventListener('keydown', onKeyDown)
-        return () => {
-            document.removeEventListener('keydown', onKeyDown)
-        }
-    })
 
     const inputContainerStyle = {
         ...localStyles.inputContainer,
@@ -59,21 +44,15 @@ export default function EditForm({
                         initialTextExtended={initialInstructions}
                         styleTheme={COMMENT_MODAL_THEME}
                         keepBreakLines={true}
+                        allowPlainEnterBreakLines={true}
+                        disabledTabKey={true}
                         disabledTags={true}
                         disabledEdition={disabled}
                     />
                 </View>
             </CustomScrollView>
             <View style={localStyles.buttonsContainer}>
-                <Button
-                    icon={'save'}
-                    iconColor={'#ffffff'}
-                    type={'primary'}
-                    onPress={done}
-                    shortcutText={'Enter'}
-                    forceShowShortcut={true}
-                    disabled={disabled}
-                />
+                <Button icon={'save'} iconColor={'#ffffff'} type={'primary'} onPress={done} disabled={disabled} />
             </View>
         </View>
     )
