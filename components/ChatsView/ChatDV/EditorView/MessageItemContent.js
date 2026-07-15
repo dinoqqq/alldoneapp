0431a11b6e51c3d058da9df56b7b5bb7308ec531
project_25604842_bot_342fa09b66be06775f693e67d20cbe2f <project_25604842_bot_342fa09b66be06775f693e67d20cbe2f@noreply.gitlab.com>
2026-07-15 13:38:43 +0000

    Open task assistant in comment popup

--- components/ChatsView/ChatDV/EditorView/MessageItemContent.js
+++ components/ChatsView/ChatDV/EditorView/MessageItemContent.js
@@ -313,14 +313,31 @@ export default function MessageItemContent({
                                 style={[localStyles.horizontalRule, !isLastLine && { marginBottom: 16 }]}
                             />
                         )
-                    } else if (/^h[1-6]$/.test(line.type)) {
-                        const headingStyle = localStyles[`header${line.type.substring(1)}`]
+                    } else if (line.type === 'h1') {
                         return (
                             <Text
                                 key={`header-${lineIndex}`}
-                                style={[headingStyle, !isLastLine && { marginBottom: 16 }]}
+                                style={[localStyles.header1, !isLastLine && { marginBottom: 16 }]}
                             >
-                                {renderFormattedText(line.segments, headingStyle)}
+                                {renderFormattedText(line.segments, localStyles.header1)}
+                            </Text>
+                        )
+                    } else if (line.type === 'h2') {
+                        return (
+                            <Text
+                                key={`header-${lineIndex}`}
+                                style={[localStyles.header2, !isLastLine && { marginBottom: 16 }]}
+                            >
+                                {renderFormattedText(line.segments, localStyles.header2)}
+                            </Text>
+                        )
+                    } else if (line.type === 'h3') {
+                        return (
+                            <Text
+                                key={`header-${lineIndex}`}
+                                style={[localStyles.header3, !isLastLine && { marginBottom: 16 }]}
+                            >
+                                {renderFormattedText(line.segments, localStyles.header3)}
                             </Text>
                         )
                     } else if (line.type === 'bullet') {
@@ -560,27 +577,6 @@ const localStyles = StyleSheet.create({
         fontFamily: 'Roboto-Medium',
         fontSize: 20,
         lineHeight: 28,
-        color: colors.Text01,
-        fontWeight: '500',
-    },
-    header4: {
-        fontFamily: 'Roboto-Medium',
-        fontSize: 18,
-        lineHeight: 26,
-        color: colors.Text01,
-        fontWeight: '500',
-    },
-    header5: {
-        fontFamily: 'Roboto-Medium',
-        fontSize: 16,
-        lineHeight: 24,
-        color: colors.Text01,
-        fontWeight: '500',
-    },
-    header6: {
-        fontFamily: 'Roboto-Medium',
-        fontSize: 14,
-        lineHeight: 20,
         color: colors.Text01,
         fontWeight: '500',
     },
