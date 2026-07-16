6c883614b45fb52fe7124cce61bf992e1a5765f4
Karsten Wysk <karsten.wysk@gmail.com>
2026-07-16 10:33:36 +0000

    Merge branch 'ai/task-list-vm-state-filter' into 'master'
    Add VM state filters to task lists
    
    See merge request alldonegmbh/alldone!92

--- components/UIComponents/FloatModals/RichCommentModal/CommentsList.js
+++ components/UIComponents/FloatModals/RichCommentModal/CommentsList.js
@@ -3,9 +3,13 @@ import { View } from 'react-native'
 
 import Comment from '../../../Feeds/FeedsModals/ListCommentsComponents/Comment'
 import { getLinkedEmailFromMessage } from '../../../ChatsView/ChatDV/linkedEmailActions'
+import VmInteractionCard from '../../../ChatsView/ChatDV/EditorView/VmInteractionCard'
+import { isAwaitingVmInteraction } from '../../../ChatsView/ChatDV/EditorView/messageLoadingState'
 
 export default function CommentsList({
     projectId,
+    objectType,
+    objectId,
     comments,
     newEmailCommentIds = new Set(),
     canArchiveLinkedEmails,
@@ -18,23 +22,39 @@ export default function CommentsList({
         <View>
             {comments.map((item, index) => {
                 const linkedEmail = getLinkedEmailFromMessage(item)
+                const showVmInteraction = isAwaitingVmInteraction(item.assistantRun)
                 return (
-                    <Comment
-                        key={item.id}
-                        comment={item}
-                        projectId={projectId}
-                        linkedEmail={linkedEmail}
-                        linkedEmailGmailData={item.gmailData}
-                        linkedEmailNew={newEmailCommentIds.has(item.id)}
-                        canArchiveLinkedEmail={canArchiveLinkedEmails}
-                        linkedEmailArchiving={archivingAllEmails || archivingEmailKeys.includes(linkedEmail?.key)}
-                        linkedEmailArchived={archivedEmailKeys.includes(linkedEmail?.key)}
-                        onArchiveLinkedEmail={onArchiveLinkedEmail}
-                        containerStyle={{
-                            marginBottom:
-                                index === 0 && comments.length > 1 ? 16 : index > 0 && comments.length > 2 ? 8 : 0,
-                        }}
-                    />
+                    <React.Fragment key={item.id}>
+                        <Comment
+                            comment={item}
+                            projectId={projectId}
+                            linkedEmail={linkedEmail}
+                            linkedEmailGmailData={item.gmailData}
+                            linkedEmailNew={newEmailCommentIds.has(item.id)}
+                            canArchiveLinkedEmail={canArchiveLinkedEmails}
+                            linkedEmailArchiving={archivingAllEmails || archivingEmailKeys.includes(linkedEmail?.key)}
+                            linkedEmailArchived={archivedEmailKeys.includes(linkedEmail?.key)}
+                            onArchiveLinkedEmail={onArchiveLinkedEmail}
+                            containerStyle={{
+                                marginBottom: showVmInteraction
+                                    ? 0
+                                    : index === 0 && comments.length > 1
+                                    ? 16
+                                    : index > 0 && comments.length > 2
+                                    ? 8
+                                    : 0,
+                            }}
+                        />
+                        {showVmInteraction && (
+                            <VmInteractionCard
+                                projectId={projectId}
+                                objectType={objectType}
+                                objectId={objectId}
+                                commentId={item.id}
+                                assistantRun={item.assistantRun}
+                            />
+                        )}
+                    </React.Fragment>
                 )
             })}
         </View>
